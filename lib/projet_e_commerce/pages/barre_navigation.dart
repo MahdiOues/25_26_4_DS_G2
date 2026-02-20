6149fd892e40d47756e9292346787f938ee1ec56
Mohamed Ben Jazia <mohamed.benjazia@polytechnicien.tn>
2026-02-20 09:48:04 +0100

    Code Source Séance 5: Création HomePage

--- lib/projet_e_commerce/pages/barre_navigation.dart
+++ lib/projet_e_commerce/pages/barre_navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class BarreNavigationScreen extends StatefulWidget {
   const BarreNavigationScreen({super.key});
 
@@ -7,10 +8,31 @@ class BarreNavigationScreen extends StatefulWidget {
 }
 
 class _BarreNavigationScreenState extends State<BarreNavigationScreen> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("TO DO ..."));
-      
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        backgroundColor: Colors.grey,
+        showSelectedLabels: true,
+        selectedLabelStyle: TextStyle(fontSize: 16, color: Colors.blue),
+        type: BottomNavigationBarType.fixed,
+
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.white,
+
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Accueil"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Liste Produits",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Mon Panier"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Favoris"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Mon Profile"),
+        ],
+      ),
+      body: Text("TO DO ..."),
+    );
   }
 }
