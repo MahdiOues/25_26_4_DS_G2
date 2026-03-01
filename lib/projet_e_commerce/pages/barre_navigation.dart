ff5ed6870bb1f355983835579be4cbe0030eb3eb
Mohamed Ben Jazia <mohamed.benjazia@polytechnicien.tn>
2026-03-01 18:10:51 +0100

    Seance 6: Correction CR1 + Création Model Produit

--- lib/projet_e_commerce/pages/barre_navigation.dart
+++ lib/projet_e_commerce/pages/barre_navigation.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:projet/projet_e_commerce/pages/favoris.dart';
+import 'package:projet/projet_e_commerce/pages/home_page.dart';
+import 'package:projet/projet_e_commerce/pages/page_user.dart';
+import 'package:projet/projet_e_commerce/pages/panier.dart';
+import 'package:projet/projet_e_commerce/pages/produit_list.dart';
 
 class BarreNavigationScreen extends StatefulWidget {
   const BarreNavigationScreen({super.key});
@@ -8,31 +13,46 @@ class BarreNavigationScreen extends StatefulWidget {
 }
 
 class _BarreNavigationScreenState extends State<BarreNavigationScreen> {
+  int selectedIndex = 0;
+  List<Widget> mesPages = [
+    HomePage(),
+    ProduitListPage(),
+    PanierPage(),
+    FavoriPage(),
+    UserInfoPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        currentIndex: selectedIndex,
         backgroundColor: Colors.grey,
         showSelectedLabels: true,
         selectedLabelStyle: TextStyle(fontSize: 16, color: Colors.blue),
         type: BottomNavigationBarType.fixed,
-
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.white,
-
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Accueil"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.list),
             label: "Liste Produits",
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Mon Panier"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Favoris"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Mon Profile"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Mon Panier",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favoris"),
+          BottomNavigationBarItem(icon: Icon(Icons.face), label: "Profil"),
         ],
       ),
-      body: Text("TO DO ..."),
+      body: mesPages[selectedIndex],
     );
   }
 }
--- lib/projet_e_commerce/pages/home_page.dart
+++ lib/projet_e_commerce/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projet/projet_e_commerce/const/images.dart';
 import 'package:projet/projet_e_commerce/myWidgets/carousel_images.dart';
+import 'package:projet/projet_e_commerce/myWidgets/image_categorie.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,6 +24,45 @@ class _MyWidgetState extends State<HomePage> {
         children: [
           //affciher uen image
           ImageCarousel(),
+          Text(
+            "Catégorie",
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.black,
+            ),
+          ),
+          //Catégories
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: List.generate(mesImagesCategories.length, (index) {
+                return ImageCategorie(
+                  titre: titreMesImagesCategories[index],
+                  urlImage: mesImagesCategories[index],
+                );
+              }),
+            ),
+          ),
+          Text(
+            "Marque",
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.black,
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: List.generate(mesImagesCategories.length, (index) {
+                return ImageCategorie(
+                  titre: titreMesImagesCategories[index],
+                  urlImage: mesImagesCategories[index],
+                );
+              }),
+            ),
+          ),
         ],
       ),
     );
